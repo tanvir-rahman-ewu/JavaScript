0384c7d9574682795bcd6211d5d1487a6b577c45
TanvirRahman <tanvir.rahman.ewu@gmail.com>
2020-06-01 20:44:41 +0600

    forEach

---
+++ forEach.js
@@ -0,0 +1,23 @@
+let objs = [
+
+    {
+        name: "Tanvir",
+        age: 25
+    },
+    {
+        name: "Moon",
+        age: 25
+    },
+    {
+        name: "Shifat",
+        age: 25
+    }
+]
+
+objs.forEach((x)=>{
+    console.log(x.name);
+})
+
+objs.forEach((x)=>{
+    console.log(x);
+})
